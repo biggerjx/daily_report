e4b4504fd85aa0d686d94018a87495d9a17ca806
yanjx <yanjx@servyo.com.cn>
2017-12-21 14:00:22 +0800

    test

---
+++ login.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+
+class login:
+
+    def __init__(self,url,username,password):
+        self.url =  url
+        self.username = username
+        self.password = password
+
+    def login(self,url,username,password):
+
